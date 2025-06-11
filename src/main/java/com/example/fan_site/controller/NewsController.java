dd0df74565c247422ea44d741502cf85f1318ecc
koheihotta191 <kobukuroayaka@gmail.com>
2025-06-11 23:20:19 +0900

    投稿フォーム作成

--- src/main/java/com/example/fan_site/controller/NewsController.java
+++ src/main/java/com/example/fan_site/controller/NewsController.java
@@ -1,4 +1,32 @@
 package com.example.fan_site.controller;
 
+import com.example.fan_site.entity.News;
+import com.example.fan_site.repository.NewsRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import java.time.LocalDateTime;
+
+@Controller
+@RequestMapping("/admin/news")
 public class NewsController {
+
+    private final NewsRepository newsRepository;
+
+    public NewsController(NewsRepository newsRepository) {
+        this.newsRepository = newsRepository;
+    }
+
+    @GetMapping("/new")
+    public String showNewsForm(Model model) {
+        model.addAttribute("news", new News());
+        return "admin/news_form";
+    }
+
+    @PostMapping("/new")
+    public String submitNews(@ModelAttribute News news) {
+        news.setPostedAt(LocalDateTime.now());
+        newsRepository.save(news);
+        return "redirect:/";
+    }
 }
